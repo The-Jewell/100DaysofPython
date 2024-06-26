c703aaa49e76e90a711c57da3302e5cd75bd7f1c
The-Jewell <jjewell1989@gmail.com>
2024-06-26 16:56:21 -0500

    Mail Merge Challenge - Day 24
    100 days of python, day 24 challenge- mail merge using with/open

---
+++ Day_24/main.py
@@ -0,0 +1,17 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+
+PLACEHOLDER = "[name]"
+with open("input/names/invited_names.txt") as invited_names:
+    names = invited_names.readlines()
+
+with open("input/letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as completed_letter:
+            completed_letter.write(new_letter)
