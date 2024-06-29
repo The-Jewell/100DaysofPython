7860162b54acfe3100615c1aeec690d8965ac1e4
The-Jewell <jjewell1989@gmail.com>
2024-06-28 19:35:01 -0500

    nato_phonetic_alphabet project
    Day 26 of 100 days of python - program that takes a word and prints out the nato phonetic alphabet for that word

---
+++ Day_26/main.py
@@ -0,0 +1,13 @@
+import pandas
+
+np_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(np_data)
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+np_dict = {row.letter: row.code for (index,row) in np_data.iterrows()}
+# print(np_dict)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [np_dict[letter] for letter in word]
+print(output_list)
