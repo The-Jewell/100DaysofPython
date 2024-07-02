157a27bc137741fa0bac841d8d2d9eec8fa9c323
The-Jewell <jjewell1989@gmail.com>
2024-07-02 09:22:39 -0500

    nato phonetic alphabet update
    added error handling for when a user enters a number instead of a letter

--- Day_26/main.py
+++ Day_26/main.py
@@ -8,6 +8,15 @@ np_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 np_dict = {row.letter: row.code for (index,row) in np_data.iterrows()}
 # print(np_dict)
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [np_dict[letter] for letter in word]
-print(output_list)
+is_running = True
+while is_running:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [np_dict[letter] for letter in word]
+    except KeyError:
+        print("Please enter only letters.")
+    else:
+        print(output_list)
+        is_running = False
+
+
